9b9a491caba28519f1f3ff37844f9f5a77e91af2
Cristina Merisoiu <cristina.merisoiu@gmail.com>
2019-09-22 03:04:54 +0200

    add details icons

---
+++ src/stories/Details.stories.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Euro from "../icons/EuroPrice";
+import Pricing from "../components/Pricing";
+import Distance from "../components/Distance";
+import Rate from "../components/Rate";
+
+export default {
+  title: "DetailsDiv"
+};
+export const DistanceShort = () => <Distance value={3} />;
+export const DistanceLong = () => <Distance value={6} />;
+
+export const RateHigh = () => <Rate value={4.8} />;
+export const RateLow = () => <Rate value={2.8} />;
+
+export const PriceLow = () => (
+  <Pricing>
+    <Euro />
+    <Euro isGrey={true} />
+    <Euro isGrey={true} />
+  </Pricing>
+);
+
+export const PriceMedium = () => (
+  <Pricing>
+    <Euro />
+    <Euro />
+    <Euro isGrey={true} />
+  </Pricing>
+);
+
+export const PriceHigh = () => (
+  <Pricing>
+    <Euro />
+    <Euro />
+    <Euro />
+  </Pricing>
+);
